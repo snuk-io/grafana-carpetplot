32d1e39cd7615d5fc69d9f2b396ff33f3a1aab17
nbmoa <moa.ziegler@snuk.io>
2017-11-13 11:49:49 +0100

    Inserted version into the plugin.json file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,32 @@
 module.exports = (grunt) => {
   require('load-grunt-tasks')(grunt);
 
+  // make access of version variable available
+  var pkgJson = require('./package.json');
+
   grunt.loadNpmTasks('grunt-execute');
   grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-exec')
 
   grunt.initConfig({
 
     clean: ['dist'],
+
+    // get version appendix from buildinfo.sh
+    exec: {
+      getVersionAppendix: {
+        cmd: './build-scripts/buildinfo.sh version_appendix',
+        callback: function (err, stdout, stderr, cb) {
+            grunt.config.set('version_appendix', stdout.replace('\n', ''));
+          }
+      },
+      getBuildtime: {
+        cmd: './build-scripts/buildinfo.sh time',
+        callback: function (err, stdout, stderr, cb) {
+            grunt.config.set('build_time', stdout.replace('\n', ''));
+          }
+      },
+    },
 
     copy: {
       src_to_dist: {
@@ -15,9 +35,24 @@ module.exports = (grunt) => {
         src: ['**/*', '!**/*.js', '!**/*.scss', '!img/**/*'],
         dest: 'dist'
       },
+      versionFile: {
+        src: 'plugin.json',
+        dest: 'dist/plugin.json',
+        options: {
+          process: function (content, srcpath) {
+            // construct the template variables used in plugin.json
+            var buildInfo = { 
+              version_string : pkgJson.version + grunt.config.get('version_appendix'),
+              build_time: grunt.config.get('build_time')
+            };
+            content = grunt.template.process(content, {data: buildInfo})
+            return content;
+          },
+        },
+      },
       pluginDef: {
         expand: true,
-        src: ['plugin.json', 'README.md'],
+        src: ['README.md'],
         dest: 'dist',
       },
       img_to_dist: {
@@ -66,7 +101,8 @@ module.exports = (grunt) => {
 
   });
 
-  grunt.registerTask('default', ['clean', 'copy:src_to_dist', 'copy:pluginDef', 'copy:img_to_dist', 'babel', 'sass']);
+  grunt.registerTask('default', ['clean', 'exec', 'copy', 'babel', 'sass']);
+//  grunt.registerTask('default', ['clean', 'copy:src_to_dist', 'copy:pluginDef', 'copy:img_to_dist', 'babel', 'sass']);
   // grunt.registerTask('clean', ['clean']);
   // grunt.registerTask('watch', ['watch']);
 };
